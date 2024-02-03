9070352669657779f5597a2a4bb6b0a6c34f799a
bongbui321 <hoang.bui.vh@gmail.com>
2024-02-02 20:48:46 -0500

    edl only

--- flash.ps1
+++ flash.ps1
@@ -1,28 +1,13 @@
-$fastboot = "platform-tools/fastboot.exe"
-if (Test-Path -path $fastboot) {
-    Write-Host 'Platform tools found';
-} else {
-    Write-Host 'Downloading platform tools';
-
-    $client = new-object System.Net.WebClient
-    $client.DownloadFile("https://dl.google.com/android/repository/platform-tools_r28.0.2-windows.zip", "platform-tools.zip")
-
-    Write-Host 'Extracting platform tools';
-    Expand-Archive -Path "platform-tools.zip" -DestinationPath "." -Force
-
-    Remove-Item "platform-tools.zip"
-}
-
-$edl = "edl/edl"
+$edl = "edl_repo/edl"
 if (Test-Path -path $edl) {
     Write-Host 'EDL tool found'
 } else {
     Write-Host "Downloading and setting up EDL"
-    git clone https://github.com/bkerler/edl.git edl
-    cd edl
-    git submodule update --depth=1 --init --recursive
+    Invoke-Expression "git clone https://github.com/bkerler/edl.git edl_repo"
+    Invoke-Expression "cd edl_repo"
+    Invoke-Expression "git submodule update --depth=1 --init --recursive"
     Invoke-Expression "python -m pip3 install requirements.txt"
-    cd ..
+    Invoke-Expression "cd .."
 
     Write-Host "Downloading and setting up UsbDk"
     $client = new-object System.Net.WebClient
@@ -31,17 +16,17 @@ if (Test-Path -path $edl) {
     Remove-Item "UsbDk_1.0.22_x86.msi"
 }
 
-Invoke-Expression "$($edl) setactiveslot a --serial"
-Invoke-Expression "$($edl) w devcfg_a devcfg.img --serial"
-Invoke-Expression "$($edl) w aop_a aop.img --serial --serial"
-Invoke-Expression "$($edl) w xbl_a xbl.img --serial"
-Invoke-Expression "$($edl) w xbl_config_a xbl_config.img --serial"
-Invoke-Expression "$($edl) w abl_a abl.img --serial"
-Invoke-Expression "$($edl) w boot_a boot.img --serial"
-Invoke-Expression "$($edl) w system_a system.img --serial"
+Invoke-Expression "$($edl) setactiveslot a"
+Invoke-Expression "$($edl) w devcfg_a devcfg.img"
+Invoke-Expression "$($edl) w aop_a aop.img"
+Invoke-Expression "$($edl) w xbl_a xbl.img"
+Invoke-Expression "$($edl) w xbl_config_a xbl_config.img"
+Invoke-Expression "$($edl) w abl_a abl.img"
+Invoke-Expression "$($edl) w boot_a boot.img"
+Invoke-Expression "$($edl) w system_a system.img"
 
-Invoke-Expression "$($fastboot) format:ext4 userdata"
-Invoke-Expression "$($fastboot) format cache"
-Invoke-Expression "$($fastboot) continue"
+Invoke-Expression "$($edl) e userdata"
+Invoke-Expression "$($edl) e cache"
+Invoke-Expression "$($edl) reset"
 
 Write-Host 'AGNOS flash successful.';
--- flash.sh
+++ flash.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-EDL_DIR=$DIR/edl
+EDL=$DIR/edl_repo/edl
 
-if [! -d  $EDL_DIR ]; then
-  git clone https://github.com/bkerler/edl edl
-  cd $EDL_DIR
+if [! -f  $EDL ]; then
+  git clone https://github.com/bkerler/edl $DIR/edl_repo
+  cd $DIR/edl_repo
   git submodule update --depth=1 --init --recursive
   python -m pip3 install -r requirements.txt
-  sudo apt purge -y modemmanager
-  cd ..
+  #sudo apt purge -y modemmanager ?
+  sudo systemctl stop ModemManager
+  cd $DIR
 fi
 
 echo "Enter your computer password if prompted"
@@ -17,26 +18,22 @@ echo "Enter your computer password if prompted"
 FLASH_SLOT="a"
 echo "Flashing slot: $FLASH_SLOT"
 
-$EDL_DIR/edl setactiveslot a
-
-edlFlash() {
-  $EDL_DIR/edl w "$@"
-}
+# this always flash to partition "a"
+$EDL --memory=ufs
+$EDL setactiveslot a
 
 # flash non-active slot
-edlFlash aop_$FLASH_SLOT aop.img
-edlFlash devcfg_$FLASH_SLOT devcfg.img
-edlFlash xbl_$FLASH_SLOT xbl.img
-edlFlash xbl_config_$FLASH_SLOT xbl_config.img
-edlFlash abl_$FLASH_SLOT abl.img
-edlFlash boot_$FLASH_SLOT boot.img
-edlFlash system_$FLASH_SLOT system.img
+$EDL w aop_$FLASH_SLOT aop.img
+$EDL w devcfg_$FLASH_SLOT devcfg.img
+$EDL w xbl_$FLASH_SLOT xbl.img
+$EDL w xbl_config_$FLASH_SLOT xbl_config.img
+$EDL w abl_$FLASH_SLOT abl.img
+$EDL w boot_$FLASH_SLOT boot.img
+$EDL w system_$FLASH_SLOT system.img
 
 
 # wipe device
-#TODO: format userdata cache in edl mode
-
-#sudo $FASTBOOT format:ext4 userdata
-#sudo $FASTBOOT format cache
+$EDL e userdata
+$EDL e cache
 
 $EDL/edl reset
